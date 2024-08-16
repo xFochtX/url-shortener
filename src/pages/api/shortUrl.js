aec2bfac97a3b7891109b50734eed30bed471027
Focht <fabian.chipana@unmsm.edu.pe>
2024-08-15 22:25:48 -0500

    Se descomentó las líneas de shortUrl.js

--- src/pages/api/shortUrl.js
+++ src/pages/api/shortUrl.js
@@ -1,6 +1,6 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   const prueba = JSON.stringify({ mensaje: 'Ola k aze' });
